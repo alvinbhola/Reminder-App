677648458c1a09cfa95d39d88bb7fd9375e54727
alvin <alvinbhola007@icloud.com>
2018-12-10 22:41:02 -0500

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,20 @@
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  li.appendChild(span);
+
+
+}
+
